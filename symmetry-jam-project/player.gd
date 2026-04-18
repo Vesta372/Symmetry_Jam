17e63eba2e767ad2645372585374e239f5fa2850
Vesta372 <munrosimone62@gmail.com>
2026-04-18 20:02:57 +1200

    initail commit
    Tiles, animations, player

---
+++ symmetry-jam-project/player.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+@export var mirror : int
+
+var speed = 50
+var direction = Vector2 (0,0)
+
+var can_jump = true
+var is_jumping = false
+var time = 10 * 60
+var jump_timer = time
+var jumped_once = false
+
+var gravity = 0.1
+
+var max_velocity = Vector2 (500,500)
+
+func _physics_process(delta: float) -> void:
+	if mirror <0:
+		animated_sprite.flip_h = true
+	else:
+		animated_sprite.flip_h = false
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+		animated_sprite.play("walking_left")
+	elif Input.is_action_pressed("move_right"):
+		direction.x = 1
+		animated_sprite.play("walking_right")
+	else:
+		direction.x = 0
+		animated_sprite.play("default")
+	
+	if Input.is_action_just_pressed("move_up") && can_jump:
+		jump()
+	
+	if !is_on_floor():
+		is_jumping = true
+		if jumped_once:
+			can_jump = false
+			print("ASS")
+		else: 
+			print("tits")
+			jumped_once = true
+		
+		if direction.y < max_velocity.y:
+			direction.y += gravity
+	else:
+		is_jumping = false
+		can_jump = true
+		jumped_once = false
+		jump_timer = time
+	
+	direction.x *= mirror
+	
+	self.velocity = speed * direction
+	self.move_and_slide()
+
+func jump():
+	if jump_timer <= 0:
+		can_jump = false
+	else:
+		jump_timer -= 1
+	direction.y = -3
